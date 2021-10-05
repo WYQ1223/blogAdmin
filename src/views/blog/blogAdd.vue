6cd64330498dc8c6dd88ba8e04e9fec7a08d7adb
WYQ1223 <1129714116@qq.com>
2021-10-05 10:31:18 +0800

    添加toast-uii库中的富文本编辑器

--- src/views/blog/blogAdd.vue
+++ src/views/blog/blogAdd.vue
@@ -1,13 +1,36 @@
 <template>
-  <div>添加文章</div>
+  <div class="app-container">
+    <div class="title">文章标题</div>
+    <el-input
+      v-model="blogForm.title"
+      placeholder="请输入文章标题"
+      clearable
+    ></el-input>
+    <div class="title">文章编辑</div>
+    <editor />
+  </div>
 </template>
 
 <script>
+import "@toast-ui/editor/dist/toastui-editor.css";
+import { Editor } from "@toast-ui/vue-editor";
 export default {
-
-}
+  components: {
+    Editor,
+  },
+  data() {
+    return {
+      blogForm: {
+        title: "",
+      },
+    };
+  },
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.title {
+  margin: 15px 0;
+  font-weight: 100;
+}
 </style>
